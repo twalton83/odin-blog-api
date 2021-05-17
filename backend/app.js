b34dd5f639740be284b10203944b67420906632f
Tatiana <hello@tatianacodes.com>
2021-05-16 18:37:20 -0700

    update dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -14,7 +17,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
 
 module.exports = app;
